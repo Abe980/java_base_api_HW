f7e03a079ab92e3c7760517151ee52e71fca1711
Abe <ai_980@inbox.ru>
2023-06-21 18:54:45 +0300

    hw5addComments

--- HW_5/src/Solution/Task1.java
+++ HW_5/src/Solution/Task1.java
@@ -14,9 +14,13 @@ public class Task1 {
     
 
     public static void addContacts(HashMap<String, ArrayList<String>> phoneBook, String contact){
+        // делим строку на массив по пробелам, первый элемент - фамилия
         String[] arrContact = contact.split(" ");
+        // проверяем существует ли фамилия в телефонной книге
         if (phoneBook.containsKey(arrContact[0])){
+            // если фамилия есть, добавляем к ней телефоны.
             for (int i=1; i<arrContact.length; i++){
+                // проверяем есть ли уже такой телефон в книге, если нет добавляем
                 if (!(phoneBook.get(arrContact[0]).contains(arrContact[i]))){
                     phoneBook.get(arrContact[0]).add(arrContact[i]);
                 }
@@ -24,8 +28,11 @@ public class Task1 {
         }
         else {
             ArrayList<String> arrLs = new ArrayList<>();
+            // добавляем фамилию в книгу
             phoneBook.put(arrContact[0], arrLs);
+            // добавляем контакты
             for (int i=1; i<arrContact.length; i++){
+                // проверка на одинаковые контакты
                 if (!(phoneBook.get(arrContact[0]).contains(arrContact[i]))){
                     phoneBook.get(arrContact[0]).add(arrContact[i]);
                 }
@@ -35,8 +42,11 @@ public class Task1 {
     }
 
     public static void printPhoneBook(HashMap<String, ArrayList<String>> phoneBook){
+        // печатаем книгу
         for (Map.Entry<String, ArrayList<String>> item : phoneBook.entrySet()){
+            // печатаем фамилию
             System.out.println(item.getKey() + ":");
+            // печатаем телефоны
             for (int i=0; i<item.getValue().size(); i++){
                 System.out.println("   " + item.getValue().get(i));
             }
